e5c23e4f470f89bb33b3381bab6df875e2e7ba9b
Marc Christophe <m@xtof.net>
2015-05-11 16:15:54 -0700

    path coloring based on speed

--- main.js
+++ main.js
@@ -23,11 +23,6 @@ $("document").ready(function() {
                 var day = moment($title);
                 var b = day.clone();
 
-                console.log(day.startOf('day').toDate());
-                console.log(day.from(b));
-                console.log(b.minutes());
-                console.log(b.hour() + " : " + b.minute());
-
                 t_data.push({
                     date: day.startOf('day').toDate(),
                     value: b.hour() + (b.minute() / 60)
@@ -41,10 +36,80 @@ $("document").ready(function() {
                     return [parseFloat(obj.attr('lat')), parseFloat(obj.attr('lon'))];
                 });
 
-                var polyline = L.polyline(points, {
-                    color: 'red',
-                    weight: 1
-                }).addTo(map);
+                //
+                var times = _.map(m, function(xxx) {
+                    var obj = $(xxx);
+                    var t = moment(obj.find("time").text());
+                    return t;
+                });
+                var durs = _.map(times, function(t, idx, l) {
+                    if (idx > 0) {
+                        return t.diff(l[idx - 1]);
+                    }
+                });
+                var dists = _.map(points, function(p, idx, l) {
+                    if (idx > 0) {
+                        var p1 = l[idx - 1];
+                        var a = p[0] - p1[0];
+                        var b = p[1] - p1[1];
+                        var c = Math.abs(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
+                        return c * 1000;
+                    }
+                });
+                var rates = _.map(dists, function(d, idx, l) {
+                    if (idx > 0) {
+                        if (durs[idx] === 0) {
+                            return 1;
+                        } else {
+                            return 10000 * (d / durs[idx]);
+                        }
+                    }
+                });
+
+                //console.log(_.min(rates), _.max(rates));
+                var scale = chroma.scale(['green', 'yellow', 'orange', 'red']).domain([0.25, 0.45]); //[_.min(rates), _.max(rates)]);
+                var polylines = _.map(rates, function(r, idx, l) {
+                    if (idx > 0) {
+                        return L.polyline([points[idx - 1], points[idx]], {
+                            color: scale(r).hex(),
+                            weight: 2,
+                            opacity: 0.5
+                        });
+                    }
+                });
+
+                polylines.shift();
+
+                // var polyline = L.polyline([
+                //     [37.810496, -122.254968],
+                //     [37.801749, -122.261607]
+                // ], {
+                //     color: 'red',
+                //     weight: 5
+                // }).addTo(map);
+
+                // polyline.on('mouseover', function(e) {
+                //     console.log(b.format('MMMM Do YYYY, h:mm:ss a'));
+                //     this.weight = 20;
+                //     this.setStyle({
+                //         color: 'red',
+                //         weight: 10
+                //     });
+                // });
+
+                // polyline.on('mouseout', function(e) {
+                //     this.setStyle({
+                //         color: 'red',
+                //         weight: 5
+                //     });
+                // });
+
+                L.featureGroup(polylines)
+                    .bindPopup('Hello world!')
+                    .on('click', function() {
+                        alert('Clicked on a group!');
+                    })
+                    .addTo(map);
 
                 loadSomething();
 
